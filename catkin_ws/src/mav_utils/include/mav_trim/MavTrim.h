5ff9a644fde7d7323fbd05110706f67e36c4073a
Devon Morris <devonmorris1992@gmail.com>
2018-02-12 16:57:50 -0700

    todays work

--- catkin_ws/src/mav_utils/include/mav_trim/MavTrim.h
+++ catkin_ws/src/mav_utils/include/mav_trim/MavTrim.h
@@ -35,18 +35,27 @@ typedef struct{
 class MavTrim
 {
 private:
-  // Ros node handles, publishers and subscribers
-  ros::NodeHandle nh_;
-
   // Ros services
   ros::ServiceServer trim_service_;
 
   // callbacks for subs
   bool trim_cb_(mav_utils::Trim::Request req, mav_utils::Trim::Response resp);
 
+public:
+  // Ros node handles, publishers and subscribers
+  ros::NodeHandle nh_;
+  Eigen::Vector3f trim;
+
+  MavTrim();
+}; //end class MavTrim
+
+struct TrimFunctor
+{
+  TrimFunctor(MavTrim* mtrim);
+
   // Functions to calculate dynamics and forces/moments
-  Vector12f dynamics(Vector12f state, Vector6f wrench);
-  Vector6f calcWrench(Eigen::Vector3f abr);
+  template <typename T>
+  Vector12f dynamics(T abr);
 
   float sigma(float alpha);
 
@@ -55,72 +64,24 @@ private:
     return (T(0) < val) - (val < T(0));
   }
 
-  // inertia matrix
-  Eigen::Matrix3f J;
-  Eigen::Matrix3f J_inv;
-
   // Trim conditions
   Eigen::Vector3f trim;
-  Eigen::Vector3f abr;
 
   // params
-  mav_params::MavParams params_;
-
-  template <typename T>
-  bool operator()(const T* const abr, T* residual) 
-  {
-    Vector6f wrench;
-    Vector12f state = Eigen::MatrixXf::Zero(12,1);
-    Vector12f state_dot = Eigen::MatrixXf::Zero(12,1);
-    Vector12f fx;
-    state << 0.0, 0.0, 0.0, // pos
-            abr[2], abr[0] + trim(2), 0.0, // heading
-            trim(0)*std::cos(abr[0])*std::cos(abr[1]), // u
-            trim(0)*std::sin(abr[0]), // v
-            trim(0)*std::sin(abr[0])*std::cos(abr[1]), // w
-            -trim(0)*std::sin(abr[0] + trim(2))/trim(1), // p
-            trim(0)*std::sin(abr[2])*std::cos(abr[0] + trim(2))/trim(1), // q
-            trim(0)*std::cos(abr[2])*std::cos(abr[0] + trim(2))/trim(1); // r
-            
-    wrench = this->calcWrench(abr);
-    fx = this->dynamics(state, wrench);
-    return true;
-  }
-  
-public:
-  MavTrim();
-}; //end class MavTrim
-
-struct TrimFunctor{
-  TrimFunctor(MavTrim* trim)
-    : mtrim_(trim){
-  }
+  mav_params::MavParams p_;
 
   template <typename T>
   bool operator()(const T* const abr, T* residual) const
   {
-    Vector6f wrench;
-    Vector12f state = Eigen::MatrixXf::Zero(12,1);
-    Vector12f state_dot = Eigen::MatrixXf::Zero(12,1);
-    Vector12f fx;
-    state << 0.0, 0.0, 0.0, // pos
-            abr[2], abr[0] + mtrim_->trim(2), 0.0, // heading
-            mtrim_->trim(0)*std::cos(abr[0])*std::cos(abr[1]), // u
-            mtrim_->trim(0)*std::sin(abr[0]), // v
-            mtrim_->trim(0)*std::sin(abr[0])*std::cos(abr[1]), // w
-            -mtrim_->trim(0)*std::sin(abr[0] + mtrim_->trim(2))/mtrim_->trim(1), // p
-            mtrim_->trim(0)*std::sin(abr[2])*std::cos(abr[0] + mtrim_->trim(2))/mtrim_->trim(1), // q
-            mtrim_->trim(0)*std::cos(abr[2])*std::cos(abr[0] + mtrim_->trim(2))/mtrim_->trim(1); // r
-            
-    wrench = mtrim_->calcWrench(abr);
-    fx = mtrim_->dynamics(state, wrench);
-    residual = state_dot - fx;
+    Vector12f xdot, fx;
+    xdot << 0.0, 0.0, trim(0)*std::sin(trim(1)), // position derivatives
+            0.0, 0.0, 0.0,  // heading derivatives
+            0.0, 0.0, trim(0)*std::cos(trim(1))/trim(2), // velocity derivatives
+            0.0, 0.0, 0.0; // angular velocity derivatives
+    fx = dynamics(abr);
+    residual[0] = xdot(0) - fx(0);
     return true;
   }
-
-private:
-    // MavTrim class to access dynamics etc
-    MavTrim* mtrim_;
 };
 
 } //end namespace
--- catkin_ws/src/mav_utils/src/mav_trim/MavTrim.cpp
+++ catkin_ws/src/mav_utils/src/mav_trim/MavTrim.cpp
@@ -3,81 +3,125 @@
 namespace mav_trim
 {
   MavTrim::MavTrim() :
-    nh_(ros::NodeHandle()),
-    params_(nh_)
+    nh_(ros::NodeHandle())
   {
     ros::NodeHandle nh_private("~");
-
-    // Create inertia matrix
-    J << params_.Jx, 0, -params_.Jxz,
-         0,     params_.Jy,        0,
-         -params_.Jxz, 0, params_.Jz;
-
-    J_inv = J.inverse();
   }
 
-  Vector12f MavTrim::dynamics(Vector12f state, Vector6f wrench)
+  template<typename T>
+  Vector12f TrimFunctor::dynamics(T abr)
   {
     Vector12f state_dot = Eigen::MatrixXf::Zero(12,1);
 
-    //state derivatives
-    Eigen::Vector3f dpos = Eigen::Vector3f::Zero();
-    Eigen::Vector3f datt = Eigen::Vector3f::Zero();
-    Eigen::Vector3f dvel = Eigen::Vector3f::Zero();
-    Eigen::Vector3f domega = Eigen::Vector3f::Zero();
+    // Gamma terms
+    // not to be confused with gamma
+    float Gamma = p_.Jx*p_.Jz - std::pow(p_.Jxz,2);
+    float Gamma1 = p_.Jxz*(p_.Jx - p_.Jy + p_.Jz)/Gamma;
+    float Gamma2 = (p_.Jz*(p_.Jz - p_.Jy) + std::pow(p_.Jxz,2))/Gamma;
+    float Gamma3 = p_.Jz/Gamma;
+    float Gamma4 = p_.Jxz/Gamma;
+    float Gamma5 = (p_.Jz - p_.Jx)/p_.Jy;
+    float Gamma6 = p_.Jxz/p_.Jy;
+    float Gamma7 = ((p_.Jx - p_.Jy)*p_.Jx + std::pow(p_.Jxz,2))/Gamma;
+    float Gamma8 = p_.Jx/Gamma;
 
-    //unpack state
-    Eigen::Vector3f pos;
-    Eigen::Vector3f att;
-    Eigen::Vector3f vel;
-    Eigen::Vector3f omega;
-    Eigen::Vector3f force;
-    Eigen::Vector3f torque;
+    // Roll rate and yaw rate coefficients
+    float C_p0 = Gamma3*p_.C_l0 + Gamma4*p_.C_n0;
+    float C_pbeta = Gamma3*p_.C_lbeta + Gamma4*p_.C_nbeta;
+    float C_pp = Gamma3*p_.C_lp + Gamma4*p_.C_np;
+    float C_pr = Gamma3*p_.C_lr + Gamma4*p_.C_nr;
+    float C_pdela = Gamma3*p_.C_ldela + Gamma4*p_.C_ndela;
+    float C_pdelr = Gamma3*p_.C_ldelr + Gamma4*p_.C_ndelr;
+    float C_r0 = Gamma4*p_.C_l0 + Gamma8*p_.C_n0;
+    float C_rbeta = Gamma4*p_.C_lbeta + Gamma8*p_.C_nbeta;
+    float C_rp = Gamma4*p_.C_lp + Gamma8*p_.C_np;
+    float C_rr = Gamma4*p_.C_lr + Gamma8*p_.C_nr;
+    float C_rdela = Gamma4*p_.C_ldela + Gamma8*p_.C_ndela;
+    float C_rdelr = Gamma4*p_.C_ldelr + Gamma8*p_.C_ndelr;
 
-    pos << state(0), state(1), state(2);
-    att << state(3), state(4), state(5);
-    vel << state(6), state(7), state(8);
-    omega << state(9), state(10), state(11);
+    // calculate trim state of the system
+    float phi = abr[2];
+    float theta = abr[2] + trim(2);
+    float u = trim(0)*std::cos(abr[0])*std::cos(abr[1]);
+    float v = trim(0)*std::sin(abr[0]);
+    float w = trim(0)*std::sin(abr[0])*std::cos(abr[1]);
+    float p = -trim(0)*std::sin(abr[0] + trim(2))/trim(1);
+    float q = trim(0)*std::sin(abr[2])*std::cos(abr[0] + trim(2))/trim(1);
+    float r = trim(0)*std::cos(abr[2])*std::cos(abr[0] + trim(2))/trim(1);
 
-    force << wrench(0), wrench(1), wrench(2); 
-    torque << wrench(3), wrench(4), wrench(5); 
+    float cphi = std::cos(phi);
+    float sphi = std::sin(phi);
+    float ctheta = std::cos(theta);
+    float stheta = std::sin(theta);
+    float ttheta = std::tan(theta);
 
-    float cphi = std::cos(att(0));
-    float sphi = std::sin(att(0));
-    float ctheta = std::cos(att(1));
-    float stheta = std::sin(att(1));
-    float ttheta = std::tan(att(1));
-    float cpsi = std::cos(att(2));
-    float spsi = std::sin(att(2));
+    // calculate aerodynamics
+    float V_a2 = std::pow(trim(0),2);
+    float AR = std::pow(p_.b,2)/p_.S;
+    float sigma_alpha = sigma(abr[0]);
+    float C_Lflat = 2*sgn(abr[0])*std::pow(std::sin(abr[0]),2)*std::cos(abr[0]);
+    float C_Lalpha = (1.0 - sigma_alpha)*(p_.C_L0 + p_.C_Lalpha*abr[0]) + sigma_alpha*C_Lflat;
+    float C_Dalpha = p_.C_Dp + std::pow(p_.C_L0 + p_.C_Lalpha*abr[0], 2.0) /
+      (M_PI*p_.e*AR);
+    float C_X = -C_Dalpha*std::cos(abr[0]) + C_Lalpha*std::sin(abr[0]); 
+    float C_Xq = -p_.C_Dq*std::cos(abr[0]) + p_.C_Lq*std::sin(abr[0]);
+    float C_Xdele = -p_.C_Ddele*std::cos(abr[0]) + p_.C_Ldele*std::sin(abr[0]);
+    float C_Z = -C_Dalpha*std::sin(abr[0]) - C_Lalpha*std::cos(abr[0]);
+    float C_Zq = -p_.C_Dq*std::sin(abr[0]) - p_.C_Lq*std::cos(abr[0]);
+    float C_Zdele = -p_.C_Ddele*std::sin(abr[0]) - p_.C_Ldele*std::cos(abr[0]);
 
+    // calculate trim inputs
+    Command command;
+    command.dele = ((p_.Jxz*(std::pow(p,2) - std::pow(r,2))+
+          (p_.Jx - p_.Jz)*p*r)
+        /(.5*p_.rho*V_a2*p_.c*p_.S) 
+        - p_.C_m0 - p_.C_malpha*abr[0] 
+        - p_.C_mq*(0.5*p_.c*q/trim(0)))/p_.C_mdele;
 
-    // Transformation to convert body into vehicle (i.e. NED)
-    // note: vehicle to body is given, then we take the inverse, but Eigen uses active rotations
-    // instead of passive rotations so we take the inverse again
-    Eigen::Quaternion<float> R_vb (Eigen::AngleAxisf(att(2), Eigen::Vector3f::UnitZ()) *
-                          Eigen::AngleAxisf(att(1), Eigen::Vector3f::UnitY()) *
-                          Eigen::AngleAxisf(att(0), Eigen::Vector3f::UnitX())); 
+    command.delt = std::sqrt(2.0*p_.m*(-r*v + q*w +
+        p_.g*std::sin(theta) - p_.rho*V_a2*p_.S*
+        (C_X+C_Xq*(.5*p_.c*q/trim(0))+C_Xdele*command.dele)) /
+        (p_.rho*p_.Sprop*p_.C_prop*std::pow(p_.k_motor,2)) + 
+        V_a2/(std::pow(p_.k_motor, 2)));
 
-    // I used this matrix to check my sanity with the quaternion
-    //Eigen::Matrix3f R_vb;
-    //R_vb << ctheta*cpsi, sphi*stheta*spsi - cphi*spsi, cphi*stheta*cpsi + sphi*spsi,
-    //     ctheta*spsi, sphi*stheta*spsi + cphi*cpsi, cphi*stheta*spsi - sphi*cpsi,
-    //     -stheta, sphi*ctheta, cphi*ctheta;
+    Eigen::Matrix2f mat;
+    mat << C_pdela, C_pdelr,
+           C_rdela, C_rdelr;
+    mat = mat.inverse().eval();
+    Eigen::Vector2f vec;
+    vec(0) = (-Gamma1*p*q + Gamma2*q*r)/(0.5*p_.rho*V_a2*p_.S*p_.b) -
+      C_p0 - C_pbeta*abr[1] - 0.5*C_pp*p_.b*p/trim(0) - 0.5*C_pr*p_.b*r/trim(0);
+    vec(1) = (-Gamma7*p*q + Gamma2*q*r)/(0.5*p_.rho*V_a2*p_.S*p_.b) -
+      C_r0 - C_rbeta*abr[1] - 0.5*C_rp*p_.b*p/trim(0) - 0.5*C_rr*p_.b*r/trim(0);
+    vec = mat*vec.eval();
 
-    // Make matrix for datt
-    Eigen::Matrix3f AttD;
+    command.dela = vec(0);
+    command.delr = vec(1);
 
-    AttD << 1.0, sphi*ttheta, cphi*ttheta,
-            0.0, cphi, -sphi,
-            0.0, sphi/ctheta, cphi/ctheta;
-
-    // Calculate derivatives
-    dpos = R_vb*vel;
-    dvel = -omega.cross(vel) + force/params_.m;
-    datt = AttD*omega;
-    domega = J_inv*(-omega.cross(J*omega)+torque);
-
-    state_dot << dpos, datt, dvel, domega;
+    state_dot(0) = 0.0;
+    state_dot(1) = 0.0;
+    state_dot(2) = u*stheta - v*sphi*ctheta - w*cphi*ctheta;
+    state_dot(3) = r*v-q*w - p_.g*stheta + 
+      0.5*p_.rho*V_a2*p_.S*(C_X + 0.5*C_Xq*(p_.c*q/trim(0)) + C_Xdele*command.dele)/p_.m +
+      0.5*p_.Sprop*p_.C_prop;
+    state_dot(4) = p*w - r*u + p_.g*ctheta*sphi + 
+          .5*p_.rho*V_a2*p_.S*(p_.C_Y0 + p_.C_Ybeta*abr[1] + 
+            0.5*p_.C_Yp*p_.b*p/trim(0) + 0.5*p_.C_Yr*p_.b*r/trim(0) + 
+            p_.C_Ydela*command.dela + p_.C_Ydelr*command.delr)/p_.m;
+    state_dot(5) = q*u - p*v + p_.g*ctheta*cphi +
+      0.5*p_.rho*V_a2*p_.S*(C_Z + 0.5*C_Zq*p_.c*q/trim(0) + C_Zdele*command.dele)/p_.m;
+    state_dot(6) = p + q*sphi*ttheta + r*cphi*ttheta;
+    state_dot(7) = q*cphi - r*sphi;
+    state_dot(8) = q*sphi/ctheta + r*cphi/ctheta;
+    state_dot(9) = Gamma1*p*q - Gamma2*q*r + .5*p_.rho*V_a2*p_.S*p_.b*
+      (C_p0 + C_pbeta*abr[1] + 0.5*C_pp*p_.b*p/trim(0) + 0.5*C_pr*p_.b*r/trim(0) + 
+       C_pdela*command.dela + C_pdelr*command.delr);
+    state_dot(10) = Gamma5*p*r - Gamma6*(std::pow(p,2) - std::pow(r,2)) +
+      0.5*p_.rho*V_a2*p_.S*p_.c*(p_.C_m0 + p_.C_malpha*abr[0] + 
+          0.5*p_.C_mq*p_.c*q/trim(0) + p_.C_mdele*command.dele)/p_.Jy;
+    state_dot(11) = Gamma7*p*q - Gamma1*q*r + 0.5*p_.rho*V_a2*p_.S*p_.b*
+      (C_r0 + C_rbeta*abr[1] + 0.5*C_rp*p_.b*p/trim(0) +
+       0.5*C_rr*p_.b*r/trim(0) + C_rdela*command.dela + C_rdelr*command.delr);
 
     return state_dot;
   }
@@ -89,7 +133,7 @@ namespace mav_trim
     ceres::Problem problem;
 
     ceres::CostFunction* cost_function =
-      new ceres::AutoDiffCostFunction<TrimFunctor, 1, 1>(new TrimFunctor);
+      new ceres::AutoDiffCostFunction<TrimFunctor, 12, 3>(new TrimFunctor(this));
 
     problem.AddResidualBlock(cost_function, nullptr, &x);
 
@@ -106,143 +150,18 @@ namespace mav_trim
     return true;
   }
 
-  Vector6f MavTrim::calcWrench(Eigen::Vector3f abr)
+  float TrimFunctor::sigma(float alpha)
   {
-    float gamma = trim(2);
-    float phi = abr(2);
-    float theta = trim(2) + abr(0);
-
-    float alpha = abr(0);
-    float beta = abr(1);
-
-    float V_a = trim(0);
-    float V_a2 = std::pow(V_a,2);
-
-    float R = trim(1);
-
-    Eigen::Vector3f linear;
-    Eigen::Vector3f angular;
-
-    linear << V_a*std::cos(alpha)*std::cos(beta),
-              V_a*std::sin(beta),
-              V_a*std::sin(alpha)*std::cos(beta);
-
-    angular << -V_a*std::sin(theta)*std::cos(gamma)/R,
-               V_a*std::sin(phi)*std::cos(theta)*std::cos(gamma)/R,
-               V_a*std::cos(phi)*std::cos(theta)*std::cos(gamma)/R;
-
-    // put in other commands
-    Command command;
-    command.dele = ((params_.Jxz*(std::pow(angular(0),2) - std::pow(angular(2),2))+
-          (params_.Jx - params_.Jz)*angular(0)*angular(2))
-        /(.5*params_.rho*std::pow(V_a,2)*params_.c*params_.S) 
-        - params_.C_m0 - params_.C_malpha*alpha 
-        - params_.C_mq*(0.5*params_.c*angular(1)/V_a))/params_.C_mdele;
-
-    // calculate gravity
-    Eigen::Vector3f Force_g = Eigen::Vector3f::Zero();
-    Force_g <<  0.0, 0.0, params_.m*params_.g;
-
-    // We do the same thing as in mav_dynamics and take the inverse because
-    // active vs passive rotations
-    Eigen::Quaternion<float> R_bv1 (Eigen::AngleAxisf(theta, Eigen::Vector3f::UnitY()) *
-              Eigen::AngleAxisf(phi, Eigen::Vector3f::UnitX()));
-    R_bv1 = R_bv1.inverse();
-
-    /*
-     * Gravitational forces
-     */
-
-    // Rotate gravity into body frame
-    Force_g << 0.0, 0.0, params_.m*params_.g;
-    Force_g = R_bv1 *Force_g;
-
-    /*
-     * Aerodynamic forces and moments
-     */
-
-    Eigen::Vector3f Force_aero = Eigen::Vector3f::Zero();
-    Eigen::Vector3f Moment_aero = Eigen::Vector3f::Zero();
-
-    // Calculate angle of attack and rotation from stability to body frame
-    // Note: we use the inverse to convert from active rotations to passive rotations
-    Eigen::Quaternion<float> R_bs(Eigen::AngleAxisf(alpha, Eigen::Vector3f::UnitY()));
-    R_bs = R_bs.inverse();
-
-    // Calculate longitudinal forces and moments
-    float AR = std::pow(params_.b,2)/params_.S;
-    float sigma_alpha = sigma(alpha);
-    float C_Lflat = 2*sgn(alpha)*std::pow(std::sin(alpha),2)*std::cos(alpha);
-    float C_Lalpha = (1.0 - sigma_alpha)*(params_.C_L0 + params_.C_Lalpha*alpha) + sigma_alpha*C_Lflat;
-    //float C_Lalpha = params_.C_L0 + params_.C_Lalpha*alpha;
-    float C_Dalpha = params_.C_Dp + std::pow(params_.C_L0 + params_.C_Lalpha*alpha, 2.0) /
-      (M_PI*params_.e*AR);
-    // float C_Dalpha = params_.C_D0 + params_.C_Dalpha*alpha;
-
-    float F_lift = .5*params_.rho*V_a2*params_.S*(C_Lalpha + 
-        params_.C_Lq*(.5*params_.c/V_a)*0*angular(1) + params_.C_Ldele*command.dele); 
-    float F_drag = .5*params_.rho*V_a2*params_.S*(C_Dalpha + 
-        params_.C_Dq*(.5*params_.c/V_a)*0*angular(1) + params_.C_Ddele*command.dele); 
-
-    Force_aero << -F_drag, 0, -F_lift;
-    // coded rotation matrix to check quaternion math
-    //Eigen::Matrix3f m;
-    //m << std::cos(alpha), 0, -std::sin(alpha),
-    //     0, 0, 0,
-    //     std::sin(alpha), 0, std::cos(alpha);
-
-    Force_aero = R_bs*Force_aero;
-
-    Moment_aero(1) = .5*params_.rho*V_a2*params_.S*params_.c*(params_.C_m0 +
-        params_.C_malpha*alpha + params_.C_mq*(.5*params_.c/V_a)*angular(1) + 
-        params_.C_mdele*command.dele);
-
-  // Calculate lateral forces and moments
-    Force_aero(1) = .5*params_.rho*V_a2*params_.S*(params_.C_Y0 +
-       params_.C_Ybeta*beta + params_.C_Yp*(.5*params_.b/V_a)*angular(0) + 
-       params_.C_Yr*(.5*params_.b/V_a)*angular(2) + params_.C_Ydela*command.dela +
-       params_.C_Ydelr*command.delr);
-
-    Moment_aero(0) = .5*params_.rho*V_a2*params_.S*params_.b*(params_.C_l0 +
-       params_.C_lbeta*beta + params_.C_lp*(.5*params_.b/V_a)*angular(0) + 
-       params_.C_lr*(.5*params_.b/V_a)*angular(2) + params_.C_ldela*command.dela +
-       params_.C_ldelr*command.delr);
-
-    Moment_aero(2) = .5*params_.rho*V_a2*params_.S*params_.b*(params_.C_n0 +
-       params_.C_nbeta*beta + params_.C_np*(.5*params_.b/V_a)*angular(0) + 
-       params_.C_nr*(.5*params_.b/V_a)*angular(2) + params_.C_ndela*command.dela +
-       params_.C_ndelr*command.delr);
-
-    /*
-     * Propulsion Forces and Moments
-     */
-    Eigen::Vector3f Force_prop = Eigen::Vector3f::Zero();
-    Eigen::Vector3f Moment_prop = Eigen::Vector3f::Zero();
-
-    Force_prop(0) = .5*params_.rho*params_.Sprop*params_.C_prop*
-      (std::pow(params_.k_motor*command.delt, 2) - V_a);
-
-    Moment_prop(0) = -params_.k_Tp*std::pow(params_.k_Omega*command.delt,2);
-
-    /*
-     * Sum of Forces and Moments
-     */
-    Eigen::Vector3f force = Eigen::Vector3f::Zero();
-    Eigen::Vector3f torque = Eigen::Vector3f::Zero();
-    force = Force_prop + Force_g + Force_aero;
-    torque = Moment_prop + Moment_aero;
-    Vector6f wrench;
-    wrench << force, torque;
-
-    return wrench;
+    return (1.0 + std::exp(-p_.M*(alpha - p_.alpha0)) 
+        + std::exp(p_.M*(alpha+p_.alpha0))) /
+      ((1.0 + std::exp(-p_.M*(alpha - p_.alpha0)))*
+       (1.0 + std::exp(p_.M*(alpha+p_.alpha0))));
   }
 
-  float MavTrim::sigma(float alpha)
+  TrimFunctor::TrimFunctor(MavTrim* mtrim):
+    p_(mtrim->nh_)
   {
-    return (1.0 + std::exp(-params_.M*(alpha - params_.alpha0)) 
-        + std::exp(params_.M*(alpha+params_.alpha0))) /
-      ((1.0 + std::exp(-params_.M*(alpha - params_.alpha0)))*
-       (1.0 + std::exp(params_.M*(alpha+params_.alpha0))));
+    trim = mtrim->trim;
   }
 
 }
